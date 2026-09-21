f3d79215f163dc50425d0484bbab721b9d669bd4
iris wen <iriswen518-alt@users.noreply.github.com>
2026-09-21 11:31:59 +0800

    Update trust-quiz 2026-09-21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VER = '20260921-113031';
+const VER = '20260921-113159';
 const CACHE = 'tq-' + VER;
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil((async () => {
